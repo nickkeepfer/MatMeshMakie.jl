3aab90d2077634f92516b93de68994af1cca0198
Documenter.jl <documenter@juliadocs.github.io>
2023-09-11 10:22:55 +0000

    build based on 61fc7cd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MatMeshMakie","category":"page"},{"location":"#MatMeshMakie","page":"Home","title":"MatMeshMakie","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MatMeshMakie.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MatMeshMakie]","category":"page"}]
+}
