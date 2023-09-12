d9b1d3e7419fe1259f9c4cddf4de652b508f6a88
Nick Keepfer <nkeepfer@gmail.com>
2023-09-12 23:31:09 +0100

    fsd

--- src/MatMeshMakie.jl
+++ src/MatMeshMakie.jl
@@ -14,5 +14,5 @@ include("Plotting.jl")
 export MtlMaterial, readMtlFile, get_face_materials, split_mesh_by_material,
        get_material_properties, get_submesh_material_properties, plot_obj_mtl,
        plot_submeshes, plot_mesh_with_materials, get_face_materials_from_mesh,
-       plot_obj_mtl
+       plot_obj_mtl, get_material_properties
 end
